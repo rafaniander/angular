5ca1751acc9b903968aa5b0e202f6fd357f10109
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-06 16:03:23 -0300

    Configuração e primeiro exemplo

---
+++ typescript/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+
+let episode: number = 4;
+console.log("This is episode", episode);
+episode += 1;
+console.log("Next episode is", episode);
+
+let favoriteDroid: string;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is", favoriteDroid);
