e658284094ab5b13b30f796d8cd5c4fccb066aa4
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-06 16:34:28 -0300

    Usando funções

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -7,3 +7,16 @@ console.log("Next episode is", episode);
 var favoriteDroid;
 favoriteDroid = 'BB-8';
 console.log("My favorite droid is", favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5, 1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
