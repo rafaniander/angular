2bf119386605e14c1a22a6bf7b226a5547db1dda
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-11 16:28:45 -0300

    Organizando os exemplos em módulos

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,28 +1,11 @@
-let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!";
-console.log(message);
+import { Spacecraft, Containership } from "./base-ships";
+import { MillenniumFalcon } from './starfighters';
 
-let episode: number = 4;
-console.log("This is episode", episode);
-episode += 1;
-console.log("Next episode is", episode);
+let ship = new Spacecraft('hyperdrive');
+ship.jumpIntoHyperspace();
 
-let favoriteDroid: string;
-favoriteDroid = 'BB-8';
-console.log("My favorite droid is", favoriteDroid);
+let falcon = new MillenniumFalcon();
+falcon.jumpIntoHyperspace();
 
-let isEnoughToBeatMF = function (parsecs: number): boolean {
-    return parsecs < 12;
-}
-
-let distance = 11;
-console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnoughToBeatMF(distance) ? 'YES' : 'NO'}`);
-
-let call = (name: string) => console.log(`Do you copy, ${name}?`);
-call('R2');
-
-function inc(speed: number, inc: number = 1): number {
-    return speed + inc;
-}
-
-console.log(`inc (5, 1) = ${inc(5, 1)}`);
-console.log(`inc (5) = ${inc(5)}`);
+let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2;
+console.log(`Is falcon good for the job? ${goodForTheJob(falcon) ? 'YES' : 'NO'}`);
--- typescript/dist/starfighters.js
+++ typescript/dist/starfighters.js
@@ -1,3 +1,4 @@
+"use strict";
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -8,17 +9,8 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var Spacecraft = /** @class */ (function () {
-    function Spacecraft(propulsor) {
-        this.propulsor = propulsor;
-    }
-    Spacecraft.prototype.jumpIntoHyperspace = function () {
-        console.log("Entering hyperspace with " + this.propulsor);
-    };
-    return Spacecraft;
-}());
-var ship = new Spacecraft('hyperdrive');
-ship.jumpIntoHyperspace();
+exports.__esModule = true;
+var base_ships_1 = require("./base-ships");
 var MillenniumFalcon = /** @class */ (function (_super) {
     __extends(MillenniumFalcon, _super);
     function MillenniumFalcon() {
@@ -35,8 +27,5 @@ var MillenniumFalcon = /** @class */ (function (_super) {
         }
     };
     return MillenniumFalcon;
-}(Spacecraft));
-var falcon = new MillenniumFalcon();
-falcon.jumpIntoHyperspace();
-var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
-console.log("Is falcon good for the job? " + (goodForTheJob(falcon) ? 'YES' : 'NO'));
+}(base_ships_1.Spacecraft));
+exports.MillenniumFalcon = MillenniumFalcon;
