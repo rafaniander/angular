a04293eceac55eb079cbfbee622e99f9fdbe11b0
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-16 14:46:47 -0300

    Implementando os itens de menu - pipe currency

--- meat-app-starter/src/app/restaurant-detail/menu/menu.component.ts
+++ meat-app-starter/src/app/restaurant-detail/menu/menu.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+
+import { MenuItem } from './../menu-item/menu-item.model';
+import { RestaurantsService } from './../../restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-menu',
@@ -6,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menu: Observable<MenuItem[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private restaurantsService: RestaurantsService
+  ) { }
 
   ngOnInit() {
+    this.menu = this.restaurantsService.menuOfRestaurant(this.route.parent.snapshot.params['id']);
+  }
+
+  addMenuItem(item: MenuItem) {
+    console.log(item);
   }
 
 }
