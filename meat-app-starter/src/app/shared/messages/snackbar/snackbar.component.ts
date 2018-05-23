734bfae018b8b8efd149fe510e1cca2234367072
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-23 16:59:45 -0300

    Usando os operadores Do e SwitchMap

--- meat-app-starter/src/app/shared/messages/snackbar/snackbar.component.ts
+++ meat-app-starter/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -4,7 +4,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
-import { timer } from 'rxjs/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
@@ -35,11 +36,13 @@ export class SnackbarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
-    });
+    this.notificationService.notifier
+      .do(message => {
+        this.message = message;
+        this.snackVisibility = 'visible';
+      })
+      .switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibility = 'hidden');
   }
 
 }
