8e542da72d0bba966091ea9b05c213b864510759
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-23 14:02:53 -0300

    Adicionando serviços a um módulo compartilhado

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 import { ROUTES } from './app.routes';
 
@@ -37,8 +36,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
