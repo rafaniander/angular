c6637a3e6fe9e2528a1c176d3997833ab5575410
Rafael Marques <rafaelmarques1992@gmail.com>
2018-05-23 11:19:18 -0300

    Criando um feature module (Módulo de Compra)

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -5,12 +5,10 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', loadChildren: './about/about.module#AboutModule' },
   { path: 'restaurants', component: RestaurantsComponent },
   {
     path: 'restaurants/:id', component: RestaurantDetailComponent,
@@ -20,6 +18,7 @@ export const ROUTES: Routes = [
       { path: 'reviews', component: ReviewsComponent }
     ]
   },
-  { path: 'order', component: OrderComponent },
-  { path: 'order-summary', component: OrderSummaryComponent }
+  { path: 'order-summary', component: OrderSummaryComponent },
+  { path: 'order', loadChildren: './order/order.module#OrderModule' },
+  { path: 'about', loadChildren: './about/about.module#AboutModule' }
 ];
