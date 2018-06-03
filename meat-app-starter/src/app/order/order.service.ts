91368f179d6596c8d542f8fb25f923845af0f409
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-03 15:50:50 -0300

    Utilizando HTTP Interceptors

--- meat-app-starter/src/app/order/order.service.ts
+++ meat-app-starter/src/app/order/order.service.ts
@@ -10,15 +10,13 @@ import { Order, OrderItem } from './order.model';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
-import { LoginService } from '../security/login/login.service';
 
 @Injectable()
 export class OrderService {
 
   constructor(
     private cartService: ShoppingCartService,
-    private http: HttpClient,
-    private loginService: LoginService
+    private http: HttpClient
   ) { }
 
   itemsValue(): number {
@@ -46,11 +44,7 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    let headers = new HttpHeaders();
-    if (this.loginService.isLoggedIn()) {
-      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
-    }
-    return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
       .map(order => order.id);
   }
 
--- meat-app-starter/src/app/shared/shared.module.ts
+++ meat-app-starter/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
@@ -14,6 +15,7 @@ import { NotificationService } from './messages/notification.service';
 import { LoginService } from '../security/login/login.service';
 import { LoggedInGuard } from '../security/loggedin.guard';
 import { LeaveOrderGuard } from '../order/leave-order.guard';
+import { AuthInterceptor } from '../security/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,8 @@ export class SharedModule {
         NotificationService,
         LoginService,
         LoggedInGuard,
-        LeaveOrderGuard
+        LeaveOrderGuard,
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
       ]
     };
   }
