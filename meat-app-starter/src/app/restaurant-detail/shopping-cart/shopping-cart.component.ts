359fd61761c87ef0f7c0612db2be297682755ce1
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-03 21:35:58 -0300

    Melhorias no processo de compilação

--- meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ meat-app-starter/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -9,18 +9,19 @@ import { ShoppingCartService } from './shopping-cart.service';
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html',
+  preserveWhitespaces: true,
   animations: [
     trigger('row', [
-      state('ready', style({opacity: 1})),
+      state('ready', style({ opacity: 1 })),
       transition('void => ready', animate('500ms 0s ease-in', keyframes([
-        style({opacity: 0, transform: 'translateX(-30px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(10px)', offset: 0.8}),
-        style({opacity: 1, transform: 'translateX(0px)', offset: 1})
+        style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 1 })
       ]))),
       transition('ready => void', animate('500ms 0s ease-in', keyframes([
-        style({opacity: 1, transform: 'translateX(0px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2}),
-        style({opacity: 1, transform: 'translateX(30px)', offset: 1})
+        style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+        style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+        style({ opacity: 1, transform: 'translateX(30px)', offset: 1 })
       ])))
     ])
   ]
